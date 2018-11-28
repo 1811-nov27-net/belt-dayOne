2dd49c6656f78256712a50daac901dbe7c068cc4
willbelt <willbeltit@gmail.com>
2018-11-28 17:03:33 -0600

    Exception handling

--- week1/day3/ExceptionHandling/ExceptionHandling/Program.cs
+++ week1/day3/ExceptionHandling/ExceptionHandling/Program.cs
@@ -6,7 +6,37 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                BadCode();
+                //never runs
+                var x = 4;
+                var y = x / 0;
+                Console.WriteLine("Never prints due to exception");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Don't Divide by Zero");
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine("Handled Bad Cast");
+            }
+            Console.WriteLine("Program continues");
+            
+        }
+
+        static void BadCode()
+        {
+            try
+            {
+                object o = true;
+                string s = (string)o;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Doesn't Print");
+            }
         }
     }
 }
