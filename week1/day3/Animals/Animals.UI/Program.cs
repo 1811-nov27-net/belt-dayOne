a237c7fc9241095797d507353819931b61035236
willbelt <willbeltit@gmail.com>
2018-11-28 14:30:15 -0600

    Added Animals project

---
+++ week1/day3/Animals/Animals.UI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Animals.library;
+
+namespace Animals.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ week1/day3/Animals/Animals.library/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Animals.library
+{
+    public class Dog
+    {
+        public string Name;
+        public string Breed { get; set;}
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                 _age = value;
+            }
+        }
+        public int Weight;
+
+        public int GetWeight()
+        {
+            return Weight;
+        }
+
+        public void SetWeight(int weight)
+        {
+            Weight = weight;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+}
