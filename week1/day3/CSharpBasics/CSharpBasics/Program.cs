61b8f2bd091431c847b2c81080f8400b894073ab
willbelt <willbeltit@gmail.com>
2018-11-28 12:30:19 -0600

    CSharpBasics notes added

--- week1/day3/CSharpBasics/CSharpBasics/Program.cs
+++ week1/day3/CSharpBasics/CSharpBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpBasics
 {
@@ -7,6 +8,66 @@ namespace CSharpBasics
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int x = 0;
+            double y = 4.58;
+            decimal z = 5.001m;
+
+            string s = "string";
+            bool b = true;
+            b = false;
+
+            object o = true; //can be ANY type
+
+            var v = "Hello";
+            //v = false; Error, type !dynamic
+
+            //control structures
+            //loops
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            while(false)
+            {
+                //while loop
+            }
+
+            do
+            {
+                //do while loop
+            } while (false);
+            if(false)
+            {
+                //if
+            }
+            else if (false)
+            {
+                //else if
+            }
+            else
+            {
+                //else
+            }
+            List<string> list = new List<string>();
+            list.Add("List contents");
+
+            foreach(var item in list)
+            {
+                //snippet: cw
+                Console.WriteLine(item);
+            }
+            // C#
+            // object-oriented
+            // part of .NET platform
+            // strongly typed (statically typed)
+            // unified type system (primitives inherit from class object)
+            // garbage collected ("managed" language)
+            // functions !first class, but close enough in practice
+            // C# somewhat functional, especially with LINQ
+            // asynchronous programming support with Task Prcessing Library
+            // exception handling
         }
     }
 }
