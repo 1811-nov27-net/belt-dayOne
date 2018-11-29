2ebf7f60a5b5f577a84bd875289d472745757c7a
willbelt <willbeltit@gmail.com>
2018-11-29 11:16:43 -0600

    Structs

---
+++ week1/day4/Structs/Structs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = new int();
+            Int32 xyz = 4;
+
+            // value types => seperate copies
+            // structs and enums == value types
+            int a = 1;
+            int b = a;
+
+            //reference types => seperate references to one copy
+            string c = "asdf";
+            string d = c;
+
+            string s = 123.ToString();
+            
+            Console.WriteLine(s);
+
+            // Don't Repeat Yourself, use methods/classes
+            // Comment: inline, multiline & XML
+            // XML for all classes & public/protected members (public API)
+            // In VS ^k ^c to comment, ^k ^u to uncomment
+            // Keep It Simple Stupid
+
+        }
+    }
+}
