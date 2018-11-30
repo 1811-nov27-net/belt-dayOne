feb2468e2c6244dd373917fc12e4e30ed2dde51a
willbelt <willbeltit@gmail.com>
2018-11-30 11:57:25 -0600

    Delegates, events, Action and Func

---
+++ week1/day5/Delegates/Delegates/MoviePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Delegates
+{
+    public class MoviePlayer
+    {
+        public string CurrentMovie { get; set; }
+
+        public delegate void MovieFinishedHandler();
+
+        public delegate void MovieFinishedStringHandler(string title);
+
+        public event MovieFinishedStringHandler MovieFinished;
+
+        public event Action<string> MovieFinished2;
+
+        public void PlayMovie()
+        {
+            Thread.Sleep(3000);
+
+            Console.WriteLine($"Finished movie {CurrentMovie}");
+
+            //MovieFinished?.Invoke();
+            if (MovieFinished != null)
+            {
+                MovieFinished(CurrentMovie);
+            }
+
+        }
+    }
+}
---
+++ week1/day5/Delegates/Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new MoviePlayer
+            {
+                CurrentMovie = "Lord of the Rings: Fellowship of the Ring Extended Edition"
+            };
+
+            MoviePlayer.MovieFinishedHandler handler = EjectDisc;
+
+            player.MovieFinished += EjectDisc;
+            // player.MovieFinished -= EjectDisc;
+
+            Action<string> handler2 = EjectDisc;
+
+            player.MovieFinished2 += handler2;
+
+            player.MovieFinished += s => Console.WriteLine("Lambda Subscribe");
+
+            player.PlayMovie();
+
+            Func<int, string, bool> func = (n, s) => true;
+
+            Func<bool> func2 = () => false;
+
+            Action<int, string, bool> func3 = (num, str, b) =>
+            {
+                if (b)
+                {
+                    Console.WriteLine(num);
+                    Console.WriteLine(str);
+                }
+            };
+
+            Action<bool> func4 = b => { return;};
+        }
+
+        static void EjectDisc()
+        {
+            Console.WriteLine("Ejecting Disc");
+        }
+
+        static void EjectDisc(string title)
+        {
+            Console.WriteLine("Ejecting Disc");
+        }
+    }
+}
