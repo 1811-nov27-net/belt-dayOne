b7515e49d73688adf10907569c1fff67d7851794
willbelt <willbeltit@gmail.com>
2018-12-06 11:51:03 -0600

    Created ADO.NETDisconnected

---
+++ week2/day4/ADO.NETDisconnected/ADO.NETDisconnected/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NETDisconnected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = SecretConfiguration.connectionString;
+
+            Console.WriteLine("Enter Name of Movie:");
+            var input = Console.ReadLine();
+            var commandString = $"SELECT * FROM Movies.Movie WHERE Name = '{input}'";
+
+            //Disconnected architecture: wait to get the whole result, load it into data set, close connection
+
+            var dataSet = new DataSet();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                //step 1: open connection
+                connection.Open();
+                // step 2: execute query
+                using (var command = new SqlCommand(commandString, connection))
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    adapter.Fill(dataSet);
+                }
+                // step 3: close connection    
+                connection.Close();
+                //step 4: process results
+                var firstTable = dataSet.Tables[0];
+                foreach(DataRow row in firstTable.Rows)
+                {
+                    object id = row["ID"];
+                    object name = row["Name"];
+                    Console.WriteLine($"ID: {id}, Name: {name}");
+                }
+            }
+        }
+    }
+}
--- week2/day4/SQLServerExampleSolution/SQLServerExample/Program.cs
+++ week2/day4/SQLServerExampleSolution/SQLServerExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace SQLServerExample
 {
@@ -12,7 +13,38 @@ namespace SQLServerExample
             //In code, use "connection string" to contain Server URL, Login, password
             var connectionString = SecretConfiguration.connectionString;
 
-            var commandString = "SELECT * FROM Movies.Movie";
+            //var commandString = "SELECT * FROM Movies.Movie";
+
+            Console.WriteLine("Enter Name of Movie:");
+            var input = Console.ReadLine();
+            var commandString = $"SELECT * FROM Movies.Movie WHERE Name = '{input}'";
+            //allows SQL injection
+            //unchecked user input should not be used to construct SQL queries, security risk
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                //step 1: open connection
+                connection.Open();
+                //step 2: Execute query
+                using (var command = new SqlCommand(commandString, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    //command.ExecuteReader() returns data
+                    //command.ExecuteNonQuery() returns int for number of rows returned
+                    //step 3: Process data
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var id = reader["ID"];
+                            var name = reader["Name"];
+                            Console.WriteLine($"ID: {id}, Name: {name}");
+                        }
+                    }
+                }
+                //step 4: close connection
+                connection.Close();
+            }
         }
     }
 }
