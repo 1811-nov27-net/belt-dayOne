1fed0250e725df70bc961639902a102be4b47568
willbelt <willbeltit@gmail.com>
2018-12-12 10:55:46 -0600

    Started

---
+++ week3/day3/ASP.NETDemo/ASP.NETDemo/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETDemo.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public List<string> Cast { get; set; }
+    }
+}
